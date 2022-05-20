07c7897c2197c20e43154b73c5a5ce972b32ee4e
David Blue <davidblue@extratone.com>
2022-05-20 06:44:27 -0500

    #1

---
+++ README.m
@@ -0,0 +1,16 @@
+# David Blue’s Editorial Configs
+
+
+### [OPEN](http://www.editorial-workflows.com/workflow/6364927996461056/GApRE_Z4wXk)
+
+Opens the selected URL in a new Safari tab. Best paired with a keyboard shortcut.
+
+### [Tweetbot Selection](http://www.editorial-workflows.com/workflow/6446859329470464/giWk4JLMKzI)
+
+Opens the selected text in Tweetbot for iOS’ compose window. Upon install, you’ll need to enter your own Twitter @ name as the `Account` variable in the Tweetbot action.
+
+Best used with a handy keyboard shortcut.
+
+### [Clear Clipboard](http://www.editorial-workflows.com/workflow/6450208867090432/4maxhAKGONg)
+
+Clears the system clipboard by copying nothing.
